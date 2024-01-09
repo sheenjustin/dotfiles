a3a73dbc6376a9783049584fb0204fcecb549576
Sheen Justin <sheen@sheenjustin.com>
2024-01-09 00:25:03 -0500

    modified setup.zsh to recursively update submodules

--- setup.zsh
+++ setup.zsh
@@ -1,5 +1,4 @@
 git clone --bare https://github.com/sheenjustin/dotfiles/ $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
-config submodule init
-config submodule update
+config submodule update --init --recursive
