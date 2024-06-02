7619a949f8c8e5e8223957c5f4c22ef131ce636c
Sheen Justin <sheenjustin@gmail.com>
2024-06-02 19:59:11 -0400

    fixed p10k right prompt padding issue

--- .config/.p10k.zsh
+++ .config/.p10k.zsh
@@ -112,6 +112,9 @@
     # example               # example user-defined segment (see prompt_example function below)
   )
 
+  # Force RPROMPT margin to 0
+  typeset -g ZLE_RPROMPT_INDENT=0
+
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
   typeset -g POWERLEVEL9K_MODE=nerdfont-v3
   # When set to `moderate`, some icons will have an extra space after them. This is meant to avoid
