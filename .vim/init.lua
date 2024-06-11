8f452bf380c7fb087b3a31a3580e86a8244bfb39
Sheen Justin <sheenjustin@gmail.com>
2024-06-10 22:01:34 -0400

    added barbar and ale plugins

--- .vim/init.lua
+++ .vim/init.lua
@@ -10,13 +10,15 @@ local Plug = vim.fn['plug#']
 Plug('nvim-lua/plenary.nvim')
 Plug('kkharji/sqlite.lua')
 Plug('nvim-tree/nvim-web-devicons')
+Plug('lewis6991/gitsigns.nvim')
+
+Plug('mrjones2014/legendary.nvim')
+Plug('folke/which-key.nvim')
 
 Plug('nvim-lualine/lualine.nvim')
 Plug('lukas-reineke/indent-blankline.nvim')
 Plug('f-person/git-blame.nvim')
-
-Plug('mrjones2014/legendary.nvim')
-Plug('folke/which-key.nvim')
+Plug('romgrk/barbar.nvim')
 
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
 Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
@@ -109,3 +111,49 @@ require("ibl").setup {
 	indent = { highlight = highlight },
 	scope = { show_exact_scope = true, highlight = { "Statement" } }
 }
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<C-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<C-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<C-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<C-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<C-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<C-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<C-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<C-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<C-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<C-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<C-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<C-c>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
+
